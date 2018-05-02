ab28e9f57da3f7b05943c97362fa95e10120d2af
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-02 19:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
+<<<<<<< HEAD
 var greetSpecialGuest = 'Hello, Neil deGrasse Tyson!';
 
 var topic = "space";
 
 var conversation = "Let's talk about space.";
+=======
+var greetSpecialGuest = 'Hello, ${specialGuest}!';
+
+var topic = "space";
+
+var conversation = 'Let\'s talk about ${topic}.';
+>>>>>>> 894a28a3e224d215be550fdb27a9b7720570d239
